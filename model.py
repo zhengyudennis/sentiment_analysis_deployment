e8a51f09a95fcb3e441aaf166c4e7bb628cff063
Yu Zheng <zhengyudennis@gmail.com>
2019-10-09 22:13:23 -0700

    Add files via upload

---
+++ model.py
@@ -0,0 +1,36 @@
+import torch.nn as nn
+
+class LSTMClassifier(nn.Module):
+    """
+    This is the simple RNN model we will be using to perform Sentiment Analysis.
+    """
+
+    def __init__(self, embedding_dim, hidden_dim, vocab_size):
+        """
+        Initialize the model by settingg up the various layers.
+        """
+        super(LSTMClassifier, self).__init__()
+
+        self.embedding = nn.Embedding(vocab_size, embedding_dim, padding_idx=0)
+        self.lstm = nn.LSTM(embedding_dim, hidden_dim)
+        #self.lstm_pre = nn.LSTM(embedding_dim, hidden_dim - 50)
+        #self.lstm = nn.LSTM(hidden_dim - 50, hidden_dim)
+        self.dense = nn.Linear(in_features=hidden_dim, out_features=1)
+        self.sig = nn.Sigmoid()
+        
+        self.word_dict = None
+
+    def forward(self, x):
+        """
+        Perform a forward pass of our model on some input.
+        """
+        x = x.t()
+        lengths = x[0,:]
+        reviews = x[1:,:]
+        embeds = self.embedding(reviews)
+        lstm_out, _ = self.lstm(embeds)
+        #lstm_pre_out, _ = self.lstm_pre(embeds)
+        #lstm_out, _ = self.lstm(lstm_pre_out)
+        out = self.dense(lstm_out)
+        out = out[lengths - 1, range(len(lengths))]
+        return self.sig(out.squeeze())
